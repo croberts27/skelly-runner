0133673ae09123c369ac2ab4ec0bf00d80f48d54
Calvin Roberts <you@example.com>
2024-02-09 19:38:46 -0700

    animations
    added walking animation for player skelly.
    doubled player sprite size

--- main.py
+++ main.py
@@ -30,56 +30,63 @@ sky_rect = sky_surf.get_rect(center=(800, 500))
 ground_surf = pygame.image.load('./images/level/ground.png').convert_alpha()
 ground_rect = ground_surf.get_rect(bottomright=(0, 730))
 
+# player surfs
+player_walk_1 = pygame.image.load('images/player/walk/walk_1.png').convert_alpha()
+player_walk_1 = pygame.transform.rotozoom(player_walk_1, 0, 2)
+player_walk_2 = pygame.image.load('images/player/walk/walk_2.png').convert_alpha()
+player_walk_2 = pygame.transform.rotozoom(player_walk_2, 0, 2)
+player_walk_3 = pygame.image.load('images/player/walk/walk_3.png').convert_alpha()
+player_walk_3 = pygame.transform.rotozoom(player_walk_3, 0, 2)
+player_walk_4 = pygame.image.load('images/player/walk/walk_4.png').convert_alpha()
+player_walk_4 = pygame.transform.rotozoom(player_walk_4, 0, 2)
+player_walk_5 = pygame.image.load('images/player/walk/walk_5.png').convert_alpha()
+player_walk_5 = pygame.transform.rotozoom(player_walk_5, 0, 2)
+player_walk_6 = pygame.image.load('images/player/walk/walk_6.png').convert_alpha()
+player_walk_6 = pygame.transform.rotozoom(player_walk_6, 0, 2)
+player_walk_7 = pygame.image.load('images/player/walk/walk_7.png').convert_alpha()
+player_walk_7 = pygame.transform.rotozoom(player_walk_7, 0, 2)
+player_walk_8 = pygame.image.load('images/player/walk/walk_8.png').convert_alpha()
+player_walk_8 = pygame.transform.rotozoom(player_walk_8, 0, 2)
+player_walk_9 = pygame.image.load('images/player/walk/walk_9.png').convert_alpha()
+player_walk_9 = pygame.transform.rotozoom(player_walk_9, 0, 2)
+player_walk = [player_walk_1, player_walk_2, player_walk_3, player_walk_4, player_walk_5, player_walk_6, player_walk_7, player_walk_8, player_walk_9]
+
+# player jump
+player_index = 0
+
+player_surf = player_walk[player_index]
+player_rect = player_surf.get_rect(midbottom=(100, 615))
+player_gravity = 0
 
 # FUNCTIONS
-def animations():
+def background_animations():
     global sky_surf, ground_surf, sky_rect, ground_rect
 
     # Move the sky horizontally
-    sky_rect.x -= 1  # Adjust the speed
+    sky_rect.x -= 1
     # If the sky has moved completely off-screen, reset its position
     if sky_rect.right <= 0:
-        sky_rect.x = 900  # Adjust the initial position based on your screen size
+        sky_rect.x = 900
     # Move the ground horizontally at a slower speed
-    ground_rect.x -= 3  # Adjust the speed as needed
+    ground_rect.x -= 3
     # If the ground has moved completely off-screen, reset its position
-    if ground_rect.x <= -90:
+    if ground_rect.x <= -85:
         ground_rect.x = 0  # Reset to the original position
 
+def player_animation():
+    global player_surf, player_index
 
+    if player_rect.bottom < 345:
+        player_surf = player_jump
+    else:
+        player_index += 0.1
+        if player_index >= len(player_walk):
+            player_index = 0
+        player_surf = player_walk[int(player_index)]
+    # player walking animation if player is on the floor
+    # display jump surface when player is not on floor
 
 # CLASSES
-class SpriteSheet(object):
-    def __init__(self, filename):
-        try:
-            self.sheet = pygame.image.load(filename).convert()
-        except pygame.error as message:
-            print('Unable to load sprite sheet image:', filename)
-            raise SystemExit(message)
-
-    # Load a specific image from a specific rectangle
-    def image_at(self, rectangle, colorkey=None):
-        """Loads image from x,y,x+offset,y+offset"""
-        rect = pygame.Rect(rectangle)
-        image = pygame.Surface(rect.size).convert()
-        image.blit(self.sheet, (0, 0), rect)
-        if colorkey is not None:
-            if colorkey == -1:
-                colorkey = image.get_at((0, 0))
-            image.set_colorkey(colorkey, pygame.RLEACCEL)
-        return image
-
-    # Load a bunch of images and return them as a list
-    def images_at(self, rects, colorkey=None):
-        """Loads multiple images, supply a list of coordinates"""
-        return [self.image_at(rect, colorkey) for rect in rects]
-
-    # Load a whole strip of images
-    def load_strip(self, rect, image_count, colorkey=None):
-        """Loads a strip of images and returns them as a list"""
-        tups = [(rect[0] + rect[2] * x, rect[1], rect[2], rect[3])
-                for x in range(image_count)]
-        return self.images_at(tups, colorkey)
 
 
 while running:
@@ -89,11 +96,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             exit()
+        if running:
+            if event.type == pygame.MOUSEBUTTONDOWN and player_rect.bottom >= 345:
+                if player_rect.collidepoint(event.pos):
+                    player_gravity = -20
+            if event.type == pygame.KEYDOWN and player_rect.bottom >= 345:
+                if event.key == pygame.K_SPACE:
+                    player_gravity = -20
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    animations()
+    background_animations()
 
     screen.fill((102, 113, 159))
 
@@ -102,6 +116,13 @@ while running:
     screen.blit(ground_surf, ground_rect)
     screen.blit(title, title_rect)
     screen.blit(title_2, title_2_rect)
+    player_gravity += 1
+    player_rect.y += player_gravity
+    if player_rect.bottom >= 615:
+        player_rect.bottom = 615
+    player_animation()
+    screen.blit(player_surf, player_rect)
+
 
 
     # flip() the display to put your work on screen
