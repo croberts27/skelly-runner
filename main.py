2a225ee7c2fe345747d463020b744e3c0f4859d7
Calvin Roberts <you@example.com>
2024-02-09 18:32:36 -0700

    achieved parallax affect  for background

--- main.py
+++ main.py
@@ -9,18 +9,35 @@ running = True
 pygame.display.set_caption("Skelly Runner")
 
 # Init surfs
-sky_surf = pygame.image.load('./images/level/bigsky.png').convert_alpha()
+sky_surf_1 = pygame.image.load('./images/level/sky1.png').convert_alpha()
+sky_surf_2 = pygame.image.load('./images/level/sky2.png').convert_alpha()
+sky_surf_3 = pygame.image.load('./images/level/sky3.png').convert_alpha()
+sky_surf_4 = pygame.image.load('./images/level/sky4.png').convert_alpha()
+sky_surf_5 = pygame.image.load('./images/level/sky5.png').convert_alpha()
+sky_index = 0
+sky_animation = [sky_surf_1, sky_surf_2, sky_surf_3, sky_surf_4, sky_surf_5]
+
+sky_surf = sky_animation[sky_index]
+sky_rect = sky_surf.get_rect(center=(800, 500))
 ground_surf = pygame.image.load('./images/level/ground.png').convert_alpha()
+ground_rect = ground_surf.get_rect(bottomright=(0, 730))
 
 
 # FUNCTIONS
-# def animations():
-#     global sky_surf, sky_surf_1, sky_surf_2, sky_index
-#
-#     sky_index += 0.1
-#     if sky_index >= len(sky_animation):
-#         sky_index = 0
-#     sky_surf = sky_animation[int(sky_index)]
+def animations():
+    global sky_surf, ground_surf, sky_rect, ground_rect
+
+    # Move the sky horizontally
+    sky_rect.x -= 1  # Adjust the speed
+    # If the sky has moved completely off-screen, reset its position
+    if sky_rect.right <= 0:
+        sky_rect.x = 900  # Adjust the initial position based on your screen size
+    # Move the ground horizontally at a slower speed
+    ground_rect.x -= 3  # Adjust the speed as needed
+    # If the ground has moved completely off-screen, reset its position
+    if ground_rect.x <= -90:
+        ground_rect.x = 0  # Reset to the original position
+
 
 
 # CLASSES
@@ -39,7 +56,7 @@ class SpriteSheet(object):
         image = pygame.Surface(rect.size).convert()
         image.blit(self.sheet, (0, 0), rect)
         if colorkey is not None:
-            if colorkey is -1:
+            if colorkey == -1:
                 colorkey = image.get_at((0, 0))
             image.set_colorkey(colorkey, pygame.RLEACCEL)
         return image
@@ -68,10 +85,14 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
 
-    # RENDER YOUR GAME HERE
+    animations()
 
-    screen.blit(sky_surf, (0, 0))
-    screen.blit(ground_surf, (0, 600))
+    screen.fill((102, 113, 159))
+
+    # RENDER YOUR GAME HERE
+    screen.blit(sky_surf, sky_rect)
+    screen.blit(ground_surf, ground_rect)
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
